342a5331b44110321473f77c5c7a11150b0df781
camilorendon <juancamilo.g.0512@gmail.com>
2022-10-04 11:12:29 -0500

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Home') }}</title>
+    <title>{{ config('app.name', 'laravel') }}</title>
 
     <meta property="og:type" content="camilo poke api" />
     <meta property="og:title" content="poke api camilo rendon" />
